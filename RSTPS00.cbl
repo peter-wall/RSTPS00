56fcf691b62cd0a633a4dacb0222e9d66da22f0d
Peter Wall <pwall@rocketsoftware.com>
2025-07-01 17:44:56 +0100

    Make transient and update .gitignore

--- RSTPS00.cbl
+++ RSTPS00.cbl
@@ -10,8 +10,10 @@
        procedure division.
 
            call 'casloadappc' using 
-             by reference              'casappc'
+             by reference              z'casappc'
            end-call
+
+           call 'mFdfh_mk_transient_dca' end-call
 
            exec cics return
            end-exec
